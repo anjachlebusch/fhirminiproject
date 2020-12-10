8b6ff58217a052b1799380fc51a6661ce1151e04
Anke Neumann <ankeneumann@Ankes-MacBook-Pro.local>
2020-12-10 14:20:45 +0100

    Delete Encounter and Appointments

--- java/hapi-fhirstarters-client-skeleton/src/main/java/fhir_project/FHIRUebung7.java
+++ java/hapi-fhirstarters-client-skeleton/src/main/java/fhir_project/FHIRUebung7.java
@@ -34,71 +34,35 @@ public class FHIRUebung7 {
        PractitionerRole doctorRole = createDoctorRole(client, doctor, doctorsOffice);
 
 
-
-      //Appointents
-       Appointment vaccine1Appointment = createAppointment(client, new GregorianCalendar(1846, Calendar.OCTOBER, 1).getTime());
-       Appointment vaccine2Appointment = createAppointment(client, new GregorianCalendar(1842, Calendar.APRIL, 12).getTime());
-       Appointment vaccine3Appointment = createAppointment(client, new GregorianCalendar(1835, Calendar.JUNE, 23).getTime());
-       Appointment vaccine4Appointment = createAppointment(client, new GregorianCalendar(1828, Calendar.JANUARY, 2).getTime());
-       Appointment vaccine5Appointment = createAppointment(client, new GregorianCalendar(1830, Calendar.SEPTEMBER, 25).getTime());
-       Appointment vaccine6Appointment = createAppointment(client, new GregorianCalendar(1852, Calendar.JULY, 12).getTime());
-
-       Appointment covidAntiGenAppointment = createAppointment(client, new GregorianCalendar(1853, Calendar.SEPTEMBER, 5).getTime());
-       Appointment roetelnAntiGenAppointment = createAppointment(client, new GregorianCalendar(1828, Calendar.FEBRUARY, 15).getTime());
-
-       //Encounter - Impfung1
-       Encounter vaccine1Encounter = createVaccineEncounter(client, vaccine1Appointment, antonie, doctor);
-
-       //Encounter - Impfung2
-       Encounter vaccine2Encounter = createVaccineEncounter(client, vaccine2Appointment, antonie, doctor);
-
-       //Encounter - Impfung3
-       Encounter vaccine3Encounter = createVaccineEncounter(client, vaccine3Appointment, antonie, doctor);
-
-       //Encounter - Impfung4
-       Encounter vaccine4Encounter = createVaccineEncounter(client, vaccine4Appointment, antonie, doctor);
-
-       //Encounter - Impfung5
-       Encounter vaccine5Encounter = createVaccineEncounter(client, vaccine5Appointment, antonie, doctor);
-
-       //Encounter - Impfung6
-       Encounter vaccine6Encounter = createVaccineEncounter(client, vaccine6Appointment, antonie, doctor);
-
-       //Encounter - Anti-Körper Röteln
-       Encounter roetelnEncounter = createAntiGenTestEncounter(client, roetelnAntiGenAppointment, antonie, doctor);
-
-       //Encounter - Anti-Körper COVID
-       Encounter covidEncounter = createAntiGenTestEncounter(client, covidAntiGenAppointment, antonie, doctor);
-
        // Impfung1
        CodeableConcept vaccineCode1 = new CodeableConcept().setCoding(Collections.singletonList(new Coding("http://hl7.org/fhir/sid/cvx", "140", "Influenza, seasonal, injectable, preservative free")));
        DateTimeType occurenceDate1 = new DateTimeType(new GregorianCalendar(1846, Calendar.OCTOBER, 1));
-       Immunization vaccine1 = createImmunization(client, antonie, doctor, vaccine1Encounter, vaccineCode1, "123456", occurenceDate1);
+       Immunization vaccine1 = createImmunization(client, antonie, doctor,  vaccineCode1, "123456", occurenceDate1);
 
        // Impfung2
        CodeableConcept vaccineCode2 = new CodeableConcept().setCoding(Collections.singletonList(new Coding("http://hl7.org/fhir/sid/cvx", "07", "mumps virus vaccine")));
        DateTimeType occurenceDate2 = new DateTimeType(new GregorianCalendar(1842, Calendar.APRIL, 12));
-       Immunization vaccine2 = createImmunization(client, antonie, doctor, vaccine2Encounter, vaccineCode2, "98765", occurenceDate2);
+       Immunization vaccine2 = createImmunization(client, antonie, doctor, vaccineCode2, "98765", occurenceDate2);
 
        // Impfung3
        CodeableConcept vaccineCode3 = new CodeableConcept().setCoding(Collections.singletonList(new Coding("http://hl7.org/fhir/sid/cvx", "67", "malaria vaccine")));
        DateTimeType occurenceDate3 = new DateTimeType(new GregorianCalendar(1835, Calendar.JUNE, 23));
-       Immunization vaccine3 = createImmunization(client, antonie, doctor, vaccine3Encounter, vaccineCode3, "98765", occurenceDate3);
+       Immunization vaccine3 = createImmunization(client, antonie, doctor, vaccineCode3, "98765", occurenceDate3);
 
        // Impfung4
        CodeableConcept vaccineCode4 = new CodeableConcept().setCoding(Collections.singletonList(new Coding("http://hl7.org/fhir/sid/cvx", "102", "DTP- Haemophilus influenzae type b conjugate and hepatitis b vaccine")));
        DateTimeType occurenceDate4 = new DateTimeType(new GregorianCalendar(1828, Calendar.JANUARY, 2));
-       Immunization vaccine4 = createImmunization(client, antonie, doctor, vaccine4Encounter, vaccineCode4, "98765", occurenceDate4);
+       Immunization vaccine4 = createImmunization(client, antonie, doctor, vaccineCode4, "98765", occurenceDate4);
 
        // Impfung5
        CodeableConcept vaccineCode5 = new CodeableConcept().setCoding(Collections.singletonList(new Coding("http://hl7.org/fhir/sid/cvx", "162", "meningococcal B vaccine, fully recombinant")));
        DateTimeType occurenceDate5 = new DateTimeType(new GregorianCalendar(1930, Calendar.SEPTEMBER, 25));
-       Immunization vaccine5 = createImmunization(client, antonie, doctor, vaccine5Encounter, vaccineCode5, "243546", occurenceDate5);
+       Immunization vaccine5 = createImmunization(client, antonie, doctor, vaccineCode5, "243546", occurenceDate5);
 
        // Impfung6
        CodeableConcept vaccineCode6 = new CodeableConcept().setCoding(Collections.singletonList(new Coding("http://hl7.org/fhir/sid/cvx", "140", "Influenza, seasonal, injectable, preservative free")));
        DateTimeType occurenceDate6 = new DateTimeType(new GregorianCalendar(1852, Calendar.JULY, 12));
-       Immunization vaccine6 = createImmunization(client, antonie, doctor, vaccine6Encounter, vaccineCode6, "123456", occurenceDate6);
+       Immunization vaccine6 = createImmunization(client, antonie, doctor, vaccineCode6, "123456", occurenceDate6);
 
        //Anti-Körper-Test - Röteln
        //TODO: Titer und Methode
@@ -106,14 +70,14 @@ public class FHIRUebung7 {
        CodeableConcept roetelnResult = new CodeableConcept(
              new Coding("http://snomed.info/sct", "260385009", "Negative (qualifier value)")
           ).setText("Schutz nicht vorhanden");
-       Observation immunizationTestRoeteln = createImmunizationTest(client, antonie, doctor, roetelnEncounter, roetelnTest, roetelnResult);
+       Observation immunizationTestRoeteln = createImmunizationTest(client, antonie, doctor, roetelnTest, roetelnResult);
 
        //Anti-Körper-Test - COVID
        CodeableConcept covidTest = new CodeableConcept(new Coding("http://loinc.org", "95209-3", "SARS-CoV+SARS-CoV-2 (COVID-19) Ag [Presence] in Respiratory specimen by Rapid immunoassay"));
        CodeableConcept covidResult = new CodeableConcept(
              new Coding("http://snomed.info/sct", "10828004", "Positive (qualifier value)")
           ).setText("Schutz vorhanden");
-       Observation immunizationTestCovid = createImmunizationTest(client, antonie, doctor, covidEncounter, covidTest, covidResult);
+       Observation immunizationTestCovid = createImmunizationTest(client, antonie, doctor, covidTest, covidResult);
        Composition c =createComposition(client,ctx,antonie,doctor,vaccine1);
     }
 
@@ -243,59 +207,15 @@ public class FHIRUebung7 {
       return appointment;
    }
 
-   private static Encounter createVaccineEncounter(IGenericClient client, Appointment appointment, Patient patient,
-                                                   Practitioner doctor) {
-      Encounter encounter = new Encounter()
-         .setStatus(Encounter.EncounterStatus.FINISHED)
-         .setClass_(new Coding("http://terminology.hl7.org/ValueSet/v3-ActEncounterCode", "AMB",
-            "A comprehensive term for health care provided in a healthcare facility (e.g. a practitioneraTMs office, clinic setting, or hospital) on a nonresident basis. The term ambulatory usually implies that the patient has come to the location and is not assigned to a bed. Sometimes referred to as an outpatient encounter."))
-         .setServiceType(
-            new CodeableConcept(new Coding("http://hl7.org/fhir/ValueSet/service-type", "57", "Immunization")))
-         .setSubject(new Reference(patient))
-         .addParticipant(new Encounter.EncounterParticipantComponent().setIndividual(new Reference(doctor)))
-         .addReasonCode(
-            new CodeableConcept(
-               new Coding("http://snomed.info/sct", "185346005", "Encounter for sign (procedure)")
-            ).setText("Immunization")
-         )
-         .addAppointment(new Reference(appointment));
 
-      MethodOutcome vaccineEncounterOutcome = createResource(client, encounter);
-      encounter.setId(vaccineEncounterOutcome.getId());
-      return encounter;
-   }
-
-   private static Encounter createAntiGenTestEncounter(IGenericClient client, Appointment appointment, Patient patient,
-                                                   Practitioner doctor) {
-      Encounter encounter = new Encounter()
-         .setStatus(Encounter.EncounterStatus.FINISHED)
-         .setClass_(new Coding("http://terminology.hl7.org/ValueSet/v3-ActEncounterCode", "AMB",
-            "A comprehensive term for health care provided in a healthcare facility (e.g. a practitioneraTMs office, clinic setting, or hospital) on a nonresident basis. The term ambulatory usually implies that the patient has come to the location and is not assigned to a bed. Sometimes referred to as an outpatient encounter."))
-         .setServiceType(
-            new CodeableConcept(new Coding("http://hl7.org/fhir/ValueSet/service-type", "162", "Pathology - Immunology")))
-         .setSubject(new Reference(patient))
-         .addParticipant(new Encounter.EncounterParticipantComponent().setIndividual(new Reference(doctor)))
-         .addReasonCode(
-            new CodeableConcept(
-               new Coding("http://snomed.info/sct", "185346005", "Encounter for sign (procedure)")
-            ).setText("anti-gen test")
-         )
-         .addAppointment(new Reference(appointment));
-
-      MethodOutcome antigentestEncounterOutcome = createResource(client, encounter);
-      encounter.setId(antigentestEncounterOutcome.getId());
-      return encounter;
-   }
-
-   private static Immunization createImmunization(IGenericClient client, Patient patient, Practitioner doctor, Encounter encounter,
+   private static Immunization createImmunization(IGenericClient client, Patient patient, Practitioner doctor,
                                                   CodeableConcept vaccineCode, String lotNumber, DateTimeType occurence) {
       Immunization vaccine = new Immunization()
          .setPatient(new Reference(patient))
          .setVaccineCode(vaccineCode)
          .setLotNumber(lotNumber)
          .setOccurrence(occurence)
-         .setPerformer(Collections.singletonList(new Immunization.ImmunizationPerformerComponent(new Reference(doctor))))
-         .setEncounter(new Reference(encounter));
+         .setPerformer(Collections.singletonList(new Immunization.ImmunizationPerformerComponent(new Reference(doctor))));
       //.setManufacturer(/*TODO: manufacturer oder Name der Impfung?*/)
 
       MethodOutcome vaccineOutcome = createResource(client, vaccine);
@@ -303,7 +223,7 @@ public class FHIRUebung7 {
       return vaccine;
    }
 
-   private static Observation createImmunizationTest(IGenericClient client, Patient patient, Practitioner performer, Encounter encounter,
+   private static Observation createImmunizationTest(IGenericClient client, Patient patient, Practitioner performer,
                                                      CodeableConcept testType, CodeableConcept testResult) {
       Observation immunizationTest = new Observation()
          .addCategory(
@@ -313,8 +233,7 @@ public class FHIRUebung7 {
          .setCode(testType)
          .setSubject(new Reference(patient))
          .addPerformer(new Reference(performer))
-         .setValue(testResult)
-         .setEncounter(new Reference(encounter));
+         .setValue(testResult);
 
       MethodOutcome immunizationTestRoetelnOutcome = createResource(client, immunizationTest);
       immunizationTest.setId(immunizationTestRoetelnOutcome.getId());
